ef83a95cdad5494e67a70f0c4e0db8c3981a1bfb
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-12-31 21:22:00 -0500

    updates

--- dev.sh
+++ dev.sh
@@ -8,8 +8,28 @@ if [[ $(docker ps -q -f name=${CONTAINER_NAME}) ]]; then
     # If the container is already running, use exec to enter the container
     echo "Container is already running, executing bash in the container."
     docker exec -it ${CONTAINER_NAME} bash
+elif [[ $(docker ps -aq -f name=${CONTAINER_NAME}) ]]; then
+    # If the container exists but is stopped, remove it
+    echo "Container exists but is stopped, removing the container."
+    docker rm ${CONTAINER_NAME}
+    # Now, start a new container with the given name
+    echo "Starting a new container."
+    docker run -it --name ${CONTAINER_NAME} \
+        --gpus all \
+        --env=NVIDIA_DRIVER_CAPABILITIES=all \
+        --env=DISPLAY=${DISPLAY} \
+        --env=XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR} \
+        --env=WAYLAND_DISPLAY=${WAYLAND_DISPLAY} \
+        --env=QT_X11_NO_MITSHM=${QT_X11_NO_MITSHM} \
+        --device=/dev/dri:/dev/dri \
+        --volume=/run/user/1000:/run/user/1000 \
+        --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+        --volume=./AD:/AD \
+        --volume=./Simulator:/Simulator \
+        ros-dev \
+        bash
 else
-    # If the container is not running, start it with the given options
+    # If the container does not exist, start it as a new one
     echo "Container is not running, starting the container."
     docker run -it --name ${CONTAINER_NAME} \
         --gpus all \
