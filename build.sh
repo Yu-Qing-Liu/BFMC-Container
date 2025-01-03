1e4e1dbe07d9a25a9b5c097b9b3d469ddb1593a2
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-02 23:57:47 -0500

    updates

--- build.sh
+++ build.sh
@@ -9,16 +9,15 @@ docker buildx build -f Dockerfile.ncnn -t ad-ncnn:latest .
 docker buildx build -f Dockerfile.acados -t ad-acados:latest .
 docker buildx build -f Dockerfile.dev -t ad-dev:latest .
 
-# Remove all intermediate images (except the final one)
-docker rmi ad-tensorrt:latest || true
-docker rmi ad-ros:latest || true
-docker rmi ad-opencv:latest || true
-docker rmi ad-vcpkg:latest || true
-docker rmi ad-ncnn:latest || true
-docker rmi ad-acados:latest || true
+# Remove all intermediate images
+docker rmi -f ad-tensorrt:latest || true
+docker rmi -f ad-ros:latest || true
+docker rmi -f ad-opencv:latest || true
+docker rmi -f ad-vcpkg:latest || true
+docker rmi -f ad-ncnn:latest || true
+docker rmi -f ad-acados:latest || true
 
-# Clean up unused containers, volumes, networks, and cache
-docker system prune -af --volumes
+docker buildx prune -f
 
 echo "Build completed"
 
--- dev.sh
+++ dev.sh
@@ -5,7 +5,6 @@ CONTAINER_NAME="ros-container"
 PLATFORM="linux/amd64"
 # Function to run the Docker container with the given parameters
 run_container() {
-    echo "Starting a new container with name ${CONTAINER_NAME}."
     docker run -it --name ${CONTAINER_NAME} \
         --platform ${PLATFORM} \
         --net=host \
@@ -21,21 +20,18 @@ run_container() {
         --volume=.:/home/admin/Repositories/ROS \
         --workdir=/home/admin/Repositories/ROS \
         ad-dev \
-        bash
+        "${@:-bash}"
 }
 # Check if the container is running
 if [[ $(docker ps -q -f name=${CONTAINER_NAME}) ]]; then
     # If the container is already running, use exec to enter the container
-    echo "Container is already running, executing bash in the container."
-    docker exec -it ${CONTAINER_NAME} bash
+    docker exec -it ${CONTAINER_NAME} "${@:-bash}"
 elif [[ $(docker ps -aq -f name=${CONTAINER_NAME}) ]]; then
     # If the container exists but is stopped, remove it
-    echo "Container exists but is stopped, removing the container."
     docker rm ${CONTAINER_NAME}
     # Now, start a new container with the given name
     run_container
 else
     # If the container does not exist, start it as a new one
-    echo "Container is not running, starting the container."
     run_container
 fi
