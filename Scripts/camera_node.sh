03bd606607b5c0aa55ca6ec315d518a65bfe63f5
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-04 02:15:41 -0500

    updates

--- Scripts/camera_node.sh
+++ Scripts/camera_node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ..
-./dev.sh bash -c "cd AD && source ~/.bashrc && source devel/setup.bash && roslaunch perception cameraNode.launch newlane:=false"
+./dev.sh bash -i -c "cd AD && source devel/setup.bash && roslaunch perception cameraNode.launch newlane:=false"
