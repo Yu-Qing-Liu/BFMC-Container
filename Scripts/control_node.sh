51486450fa6a1efa67dc60a52f0f3d314c8e19b2
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-01-19 18:31:00 -0500

    updates

--- Scripts/control_node.sh
+++ Scripts/control_node.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ..
-./dev.sh bash -i -c "cd AD && source devel/setup.bash && roslaunch control controller.launch debug_level:=2 sign:=true v:=25 dashboard:=false use_tcp:=true"
+./dev.sh bash -i -c "cd AD && source devel/setup.bash && roslaunch control controller.launch debug_level:=2 sign:=true v:=25 dashboard:=true use_tcp:=true"
