c72e55fbd5fe4988d1de00a94b44a372e14ae58f
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-02-01 12:36:07 -0500

    updates

--- Scripts/compileAD.sh
+++ Scripts/compileAD.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ..
-./dev.sh bash -i -c "cd AD && catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && ln -s build/compile_commands.json compile_commands.json"
+./dev.sh bash -i -c "cd AD && catkin_make -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && ln -s build/compile_commands.json compile_commands.json"
