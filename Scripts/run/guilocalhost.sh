eb3d979b1c20a04f50c9d8f9152dd413211844fc
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-03-01 18:41:13 -0500

    updates

--- Scripts/run/guilocalhost.sh
+++ Scripts/run/guilocalhost.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ../../
-./dev.sh zsh -i -c "cd AD && source devel/setup.zsh && python3 ./src/gui/scripts/gui.py --use_tcp"
+./dev.sh zsh -i -c "cd AD && source devel/setup.zsh && python3 ./src/gui/scripts/main.py"
